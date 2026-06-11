ecbdd011077c97d0c7cd621acd27e97595fdf283
Rahmon Faruq Babatunde <146723945+faqbaba@users.noreply.github.com>
2026-06-11 10:58:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+    "Thank you for visiting FAQ-SATROM ENTERPRISES LTD. We look forward to serving you.";
+}
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML =
+    task + ' <button onclick="this.parentElement.remove()">Delete</button>';
+
+    li.onclick = function() {
+        li.style.textDecoration = "line-through";
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    let btn = document.getElementById("darkBtn");
+
+    if (document.body.classList.contains("dark-mode")) {
+        btn.innerHTML = "☀️ Light Mode";
+    } else {
+        btn.innerHTML = "🌙 Dark Mode";
+    }
+}
