6880e253e961755a13d4cb226d66ce309bc2b8d4
faqbaba <faqbaba01@gmail.com>
2026-06-11 11:18:24 +0100

    Updated Dark Mode and To-Do List

---
+++ script.js
@@ -0,0 +1,48 @@
+function showMessage() {
+    document.getElementById("message").innerHTML =
+    "Thank you for visiting FAQ-SATROM ENTERPRISES LTD. We look forward to serving you.";
+}
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if (task === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    let li = document.createElement("li");
+
+    li.innerHTML =
+    task + ' <button onclick="this.parentElement.remove()">Delete</button>';
+
+    li.onclick = function() {
+        li.style.textDecoration = "line-through";
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    let btn = document.getElementById("darkBtn");
+
+    if (document.body.classList.contains("dark-mode")) {
+        btn.innerHTML = "☀️ Light Mode";
+        localStorage.setItem("theme", "dark");
+    } else {
+        btn.innerHTML = "🌙 Dark Mode";
+        localStorage.setItem("theme", "light");
+    }
+}
+
+window.onload = function() {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        document.getElementById("darkBtn").innerHTML = "☀️ Light Mode";
+    }
+}
