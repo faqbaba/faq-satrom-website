d51692f473dd799a22807fc21c07670ebf5f02af
faqbaba <faqbaba01@gmail.com>
2026-06-14 16:33:26 +0100

    Added homepage image slider

--- script.js
+++ script.js
@@ -65,3 +65,19 @@ function topFunction() {
         behavior: "smooth"
     });
 }
+// Hero Slider
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+
+    currentSlide++;
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    slides[currentSlide].classList.add("active");
+}
+
+setInterval(showSlide, 4000);
